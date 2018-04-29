1a5092ac3602a2cda7b2c0a54e36e24f8d2b3a14
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-04-29 20:02:13 +0100

    Mejora formato configuraciones

--- app/about.js
+++ app/about.js
@@ -27,7 +27,7 @@
  * @type {{version: string, date: string, author: {name: string, tag: string, email: string, website: string, github: string}, productname: string, productnamefooter: string, productsource: string}}
  */
 var aboutinfo = {
-    "version": "3.24",
+    "version": "3.25",
     "date": "29/04/2018",
     "author": {
         "name": "Pablo Pizarro R.",
