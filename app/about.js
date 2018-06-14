74a30f3ddaadeab8c9dfd895e49a3044db20baaa
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-06-14 21:55:15 +0100

    Corrige error íconos otros releases

--- app/about.js
+++ app/about.js
@@ -27,7 +27,7 @@
  * @type {{version: string, date: string, author: {name: string, tag: string, email: string, website: string, github: string}, productname: string, productnamefooter: string, productsource: string}}
  */
 var aboutinfo = {
-    "version": "4.08",
+    "version": "4.09",
     "date": "14/06/2018",
     "author": {
         "name": "Pablo Pizarro R.",
