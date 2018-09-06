6dd5ebb2d768eab72ca1525f7e39c209079c1e0b
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-09-06 15:04:46 +0100

    Desactiva cursor en el header

--- app/about.js
+++ app/about.js
@@ -26,8 +26,8 @@
  * @type {{version: string, date: string, author: {name: string, tag: string, email: string, website: string, github: string}, productname: string, productnamefooter: string, productsource: string}}
  */
 let aboutinfo = {
-    "version": "4.34",
-    "date": "05/09/2018",
+    "version": "4.35",
+    "date": "06/09/2018",
     "author": {
         "name": "Pablo Pizarro R.",
         "tag": "@ppizarror",
