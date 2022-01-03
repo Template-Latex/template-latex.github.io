f8c7808906413dbc7da58be2aae44d6f48cf114a
ppizarror <pablo@ppizarror.com>
2022-01-03 14:25:23 -0300

    Actualiza el contador de descargas

--- app/init.js
+++ app/init.js
@@ -1,7 +1,7 @@
 /**
  The MIT License (MIT)
 
- Copyright 2017-2021 Pablo Pizarro R.
+ Copyright 2017 Pablo Pizarro R.
 
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
@@ -179,6 +179,8 @@ $(function () {
                     }
                 }
             }
+            addDownloadCounter(total_downloads);
+            console.log(String.format('Total descargas: {0}', total_downloads));
 
             /**
              * Se añade link estadísticas a banner descargas
--- app/utils.js
+++ app/utils.js
@@ -1,7 +1,7 @@
 /**
  The MIT License (MIT)
 
- Copyright 2017-2021 Pablo Pizarro R.
+ Copyright 2017 Pablo Pizarro R.
 
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
@@ -25,21 +25,21 @@
 /**
  * Genera un número aleatorio entero entre min y max.
  *
- * @param {number} min - Mínimo
- * @param {number} max - Máximo
+ * @param {number} $min - Mínimo
+ * @param {number} $max - Máximo
  * @return {number}
  */
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt($min, $max) {
+    return Math.floor(Math.random() * ($max - $min + 1)) + $min;
 }
 
 /**
  * Selecciona el texto de un elemento.
  *
- * @param {string} elem - Elemento
+ * @param {string} $elem - Elemento
  */
-function selectAllText(elem) {
-    $(elem).on('mouseup', function () {
+function selectAllText($elem) {
+    $($elem).on('mouseup', function () {
         let sel, range;
         let el = $(this)[0];
         if (window.getSelection && document.createRange) {
@@ -74,12 +74,12 @@ function selectAllText(elem) {
  * @return {string}
  */
 function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    let $letters = '0123456789ABCDEF';
+    let $color = '#';
+    for (let $i = 0; $i < 6; $i++) {
+        $color += $letters[Math.floor(Math.random() * 16)];
     }
-    return color;
+    return $color;
 }
 
 /**
@@ -88,23 +88,23 @@ function getRandomColor() {
  * @return {string}
  */
 function getRandomDarkColor() {
-    let letters = '012345678';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * letters.length)];
+    let $letters = '012345678';
+    let $color = '#';
+    for (let $i = 0; $i < 6; $i++) {
+        $color += $letters[Math.floor(Math.random() * $letters.length)];
     }
-    return color;
+    return $color;
 }
 
 /**
  * Función String.format(...)
  */
 if (!String.format) {
-    String.format = function (format) {
-        let args = Array.prototype.slice.call(arguments, 1);
-        return format.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] !== 'undefined' ?
-                args[number] :
+    String.format = function ($format) {
+        let $args = Array.prototype.slice.call(arguments, 1);
+        return $format.replace(/{(\d+)}/g, function (match, number) {
+            return typeof $args[number] !== 'undefined' ?
+                $args[number] :
                 match;
         });
     };
@@ -113,25 +113,25 @@ if (!String.format) {
 /**
  * Desactiva cualquier elemento por id.
  *
- * @param {string} idelem - ID del elemento
+ * @param {string} $id_elem - ID del elemento
  */
-function hide_element_id(idelem) {
+function hide_element_id($id_elem) {
     try {
         // noinspection JSValidateTypes
-        document.getElementById(idelem).style = 'display:none';
+        document.getElementById($id_elem).style = 'display:none';
     } catch (err) {
-        console.log('Error al ocultar id: ' + idelem);
+        console.log('Error al ocultar id: ' + $id_elem);
     }
 }
 
 /**
  * Lanza un error, oculta contenido y muestra un div con información.
  *
- * @param {object} error - Texto del error
+ * @param {object} $error - Texto del error
  */
-function throwError(error) {
+function throwError($error) {
     // noinspection JSUnresolvedVariable
-    console.log(String.format('ERROR[{0}]: {1}', error.code, error.msg));
+    console.log(String.format('ERROR[{0}]: {1}', $error.code, $error.msg));
     $('#whatsnew').attr('style', 'display:none');
     hide_element_id('download-button');
     hide_element_id('download-button-1file');
@@ -142,10 +142,10 @@ function throwError(error) {
     let $html_section = $('#main-content-section');
     let html_error_div = '<div class="tooltip error_msg_1"><div id="errorMsgText"><!--suppress HtmlUnknownTarget --><img src="res/ui/erroricon.png" alt=""/>{0}</div><div></div></div>';
     // noinspection JSUnresolvedVariable
-    $html_section.html(String.format(html_error_div, error.msg));
+    $html_section.html(String.format(html_error_div, $error.msg));
     $('.error_msg_1').tooltipster({
         animation: 'grow',
-        content: error.moreinfo,
+        content: $error.moreinfo,
         side: 'bottom',
         theme: 'tooltipster-borderless'
     });
@@ -170,41 +170,41 @@ function throwException($e) {
 /**
  * Obtiene parámetros de la url.
  *
- * @param name
+ * @param $name
  * @return {*}
  */
-$.urlParam = function (name) {
-    let results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results == null) {
+$.urlParam = function ($name) {
+    let $results = new RegExp('[\?&]' + $name + '=([^&#]*)').exec(window.location.href);
+    if ($results == null) {
         return null;
     } else {
-        return decodeURI(results[1]) || 0;
+        return decodeURI($results[1]) || 0;
     }
 };
 
 /**
  * Retorna verdadero si el objeto no es nulo e indefinido.
  *
- * @param {object} obj - Objeto a comprobar
+ * @param {object} $obj - Objeto a comprobar
  * @returns {boolean} - Booleano de comprobación
  */
-function notNullUndf(obj) {
-    return obj !== null && obj !== undefined;
+function notNullUndf($obj) {
+    return $obj !== null && $obj !== undefined;
 }
 
 /**
  * Elige una propiedad random desde un objeto.
  *
- * @param obj - Objeto
+ * @param $obj - Objeto
  * @return {*}
  */
-function pickRandomProperty(obj) {
-    let result;
-    let count = 0;
-    for (let prop in obj)
-        if (Math.random() < 1 / ++count)
-            result = prop;
-    return result;
+function pickRandomProperty($obj) {
+    let $result;
+    let $count = 0;
+    for (let $prop in $obj)
+        if (Math.random() < 1 / ++$count)
+            $result = $prop;
+    return $result;
 }
 
 /**
@@ -286,9 +286,51 @@ function loadEncuesta() {
 }
 
 /**
- * Retorna un elemento random de una lista
+ * Retorna un elemento random de una lista.
+ *
  * @returns {*}
  */
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
 };
+
+/**
+ * Agrega un contador de descargas.
+ *
+ * @param {number} $total - Total de descargas
+ */
+function addDownloadCounter($total) {
+    let $size = 210;
+    if ($total < 100) {
+        $size = 150;
+    }
+    if ($total >= 1000) {
+        let $factor = 1;
+        if ($total <= 10000) $factor = 10;
+        $total = (Math.round($total * $factor / 1000) / $factor).toString() + 'k';
+    } else {
+        $total = $total.toString();
+    }
+    $('#templatestats').html(`
+    <svg xmlns="http://www.w3.org/2000/svg" width="98" height="20" role="img" aria-label="Descargas: ${$total}">
+       <title>Descargas: ${$total}</title>
+       <linearGradient id="s" x2="0" y2="100%">
+          <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+          <stop offset="1" stop-opacity=".1"/>
+       </linearGradient>
+       <clipPath id="r">
+          <rect width="98" height="20" rx="3" fill="#fff"/>
+       </clipPath>
+       <g clip-path="url(#r)">
+          <rect width="67" height="20" fill="#555"/>
+          <rect x="67" width="31" height="20" fill="#4c1"/>
+          <rect width="98" height="20" fill="url(#s)"/>
+       </g>
+       <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
+          <text aria-hidden="true" x="345" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="${370 + $size}">Descargas</text>
+          <text x="345" y="140" transform="scale(.1)" fill="#fff" textLength="570">Descargas</text>
+          <text aria-hidden="true" x="815" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="${$size}">${$total}</text>
+          <text x="815" y="140" transform="scale(.1)" fill="#fff" textLength="${$size}">${$total}</text>
+       </g>
+    </svg>`);
+}
